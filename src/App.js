60eea9a14439859af57d7bb41aea125063c26b5c
Sourav <mehtas018@gmail.com>
2023-07-16 23:08:25 +0530

    Understanding UseStates

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [inputVal,setInputVal] = useState("the text here changes");
+  let Change = (event) =>{
+    setInputVal(event.target.value);
+  }
+  const [counter,setCounter] = useState(0);
+  let Click = () =>{
+    setCounter(counter+1);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Learning Use States </h1>
+      <input type="text" onChange= {Change}></input>
+        {inputVal}
+        <br></br>
+      {counter}
+      <button onClick={Click}> counters</button>
     </div>
   );
 }
