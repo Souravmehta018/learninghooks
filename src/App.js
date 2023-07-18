0846e2f685eee8976935a330c5baa686bc9c3aaf
Sourav <mehtas018@gmail.com>
2023-07-18 10:42:14 +0530

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,29 +1,49 @@
 //import logo from './logo.svg';
-import { useState } from 'react';
+//import { useState } from 'react';
 import './App.css';
+import { useReducer } from 'react';
 
-function App() {
-
-  const [inputVal,setInputVal] = useState("the text here changes");
-  let Change = (event) =>{
-    setInputVal(event.target.value);
+const reducer = (allStates, action) =>{
+  switch(action.type){
+    case 'increment':
+            return {...allStates, count: allStates.count + 1};
+          case 'decrement':
+            return {...allStates, count: allStates.count - 1};
+          case'toggleShowText':
+            return {...allStates, showText:!allStates.showText};
+          default:
+            return allStates;
   }
-  const [counter,setCounter] = useState(0);
-  let Click = () =>{
-    setCounter(counter+1);
-  }
-  return (
-    <div className="App">
-      <h1> Learning Use States </h1>
-      <input type="text" onChange= {Change}></input>
-        {inputVal}
-        <br/>
-        <br/>        
-      {counter}
-      <button onClick={Click}> counters</button>
-    </div>
-    
-  )
 }
+function App() {
+  const [allStates, dispatch] = useReducer(reducer, {count: 0 , showText: true});
+const clicked = () =>{
+    dispatch({type: 'increment'});
+    dispatch({type: 'toggleShowText'});
+}
+  return(
+    <div className="app">
+      <h1>{allStates.count}</h1>
+      <button onClick={clicked}> Click here </button>
+      {allStates.showText && <p> This is the text </p>}
+    </div>
+  )
+  }
+
+  // const [cntr, setCntr] = useState(0)
+  // const [text, setText] = useState(true)
+  // let onClick = () => {
+//       setCntr(cntr + 1)
+//       setText(!text)
+//   }
+
+// return (
+//   <div>
+//       {cntr}
+//       <button onClick={onClick}>Click</button>
+//       {text && <p> This is the text </p>} {/*If text=true?"this is the text" : " " */}
+//   </div>
+// )
+
 
 export default App;
--- src/Hooks/useReducr.js
+++ src/Hooks/useReducr.js
@@ -2,21 +2,33 @@ import React, { useState } from 'react'
 
 function useReducr() {
 
-    const [cntr, setCntr] = useState(0)
-    const [text, setText] = useState(true)
-    let onClick = () => {
-        setCntr(cntr + 1)
-        setText(!text)
-    }
-
-  return (
-    <div>
-        {cntr}
-        <button onClick={onClick}>Click</button>
-        {text && <p> This is the text </p>} {/*If text=true?"this is the text" : " " */}
-        <p> "dhfjfcgvjkhbj"</p>
+  const [allStates, dispatch] = useReducer(reducer, {count: 0 , showText: true});
+const clicked = () =>{
+    dispatch({type: 'increment'});
+    dispatch({type: 'toggleShowText'});
+}
+  return(
+    <div className="app">
+      <h1>{allStates.count}</h1>
+      <button onClick={clicked}> Click here </button>
+      {allStates.showText && <p> This is the text </p>}
     </div>
   )
+  //   const [cntr, setCntr] = useState(0)
+  //   const [text, setText] = useState(true)
+  //   let onClick = () => {
+  //       setCntr(cntr + 1)
+  //       setText(!text)
+  //   }
+
+  // return (
+  //   <div>
+  //       {cntr}
+  //       <button onClick={onClick}>Click</button>
+  //       {text && <p> This is the text </p>} {/*If text=true?"this is the text" : " " */}
+  //       <p> "dhfjfcgvjkhbj"</p>
+  //   </div>
+  // )
 }
 
 export default useReducr
