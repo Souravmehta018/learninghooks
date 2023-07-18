f03f9af82812a5b6a049e4ce5d1cc27a622958fa
Sourav <mehtas018@gmail.com>
2023-07-18 15:49:19 +0530

    useReducer done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
 //import logo from './logo.svg';
-//import { useState } from 'react';
+import { useState } from 'react';
 import './App.css';
 
 
 
 function App() {
+
   }
-
-  // const [cntr, setCntr] = useState(0)
-  // const [text, setText] = useState(true)
-  // let onClick = () => {
-//       setCntr(cntr + 1)
-//       setText(!text)
-//   }
-
-// return (
-//   <div>
-//       {cntr}
-//       <button onClick={onClick}>Click</button>
-//       {text && <p> This is the text </p>} {/*If text=true?"this is the text" : " " */}
-//   </div>
-// )
-
 
 export default App;
---
+++ src/Hooks/useState.js
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import './App.css';
+
+function useState() {
+
+  const [inputVal,setInputVal] = useState("the text here changes");
+  let Change = (event) =>{
+    setInputVal(event.target.value);
+  }
+  const [counter,setCounter] = useState(0);
+  let Click = () =>{
+    setCounter(counter+1);
+  }
+  return (
+    <div className="App">
+      <h1> Learning Use States </h1>
+      <input type="text" onChange= {Change}></input>
+        {inputVal}
+        <br/>
+        <br/>        
+      {counter}
+      <button onClick={Click}> counters</button>
+    </div>
+    
+  )
+}
+
+export default useState;
